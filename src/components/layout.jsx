f7d2818c9f3abcf59fa1eda1965525ad70e6e2c7
Alexandre CMC Souza <ale@alemacedo.com>
2021-12-19 00:39:29 +0100

    update meta tags

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -7,6 +7,8 @@ const Layout = props => {
             <Helmet>
                 <meta charSet="utf-8" />
                 <meta name='description' content='Gheama is a Georgian restaurant in Milan'/>
+                <meta name="keywords" content="Gheama, Restaurant, Georgian Cuisine"></meta>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
                 <title>Gheama Restaurant</title>
             </Helmet>
             {props.children}
